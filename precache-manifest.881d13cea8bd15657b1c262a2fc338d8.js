26765f64c330d3b16de4aafeecb735d62a5016e8
Valeria Isaeva <valevs.isv@gmail.com>
2021-02-11 22:35:57 +0300

    Updates

---
+++ precache-manifest.881d13cea8bd15657b1c262a2fc338d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1881476baef8ea7f4c2be02e83b09e6d",
+    "url": "/restaurant-menu/index.html"
+  },
+  {
+    "revision": "0f700132e7f097420bb8",
+    "url": "/restaurant-menu/static/css/main.714c7c09.chunk.css"
+  },
+  {
+    "revision": "6cc9540664a807f0a261",
+    "url": "/restaurant-menu/static/js/2.963f1a57.chunk.js"
+  },
+  {
+    "revision": "0f700132e7f097420bb8",
+    "url": "/restaurant-menu/static/js/main.f468215f.chunk.js"
+  },
+  {
+    "revision": "b20dd83e36889b5892dd",
+    "url": "/restaurant-menu/static/js/runtime~main.07df0635.js"
+  },
+  {
+    "revision": "698e5e807daf0026f6ac590ce288822c",
+    "url": "/restaurant-menu/static/media/food-bg.698e5e80.jpg"
+  },
+  {
+    "revision": "701d6d1d57f5d528c4db1e887ff29e82",
+    "url": "/restaurant-menu/static/media/shopping-cart-solid.701d6d1d.svg"
+  }
+]);
